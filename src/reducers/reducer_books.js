737f956118a111749d799b7722e672b1c7513022
hongloans9 <nguyenloan150492@gmail.com>
2017-10-07 00:28:04 +0300

    first

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function (){
+    return [
+        {title: 'javascipt' },
+        {title: 'harry poster' },
+        {title: 'the dark tower' },
+        {title: 'new moon' }
+    ]
+}
